a61844a010a50281d35d2925975b1ea3d171d05a
Mikael Nensén <cybrosys@hotmail.com>
2015-09-18 17:20:42 +0200

    Created project and first simple implementation of Factory with unit tests.

---
+++ Factory/Factory.Shared/Factory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class Factory
+    {
+        public static T Get<T>(string id)
+            where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
