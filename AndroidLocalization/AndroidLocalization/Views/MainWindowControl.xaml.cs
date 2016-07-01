944cdcd72b5e9b84a247ca58a6be42c37a363104
Mikael Nensén <cybrosys@hotmail.com>
2016-07-02 01:21:46 +0200

    Was finally able to get a trigger to change the cell background color without breaking everything.

--- AndroidLocalization/AndroidLocalization/Views/MainWindowControl.xaml.cs
+++ AndroidLocalization/AndroidLocalization/Views/MainWindowControl.xaml.cs
@@ -6,11 +6,15 @@
 
 namespace AndroidLocalization.Views
 {
+    using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Media;
     using ViewModels;
-    
+
     public partial class MainWindowControl : UserControl
     {
         public MainViewModel ViewModel => DataContext as MainViewModel;
@@ -18,6 +22,20 @@ namespace AndroidLocalization.Views
         public MainWindowControl()
         {
             InitializeComponent();
+        }
+
+        private void DataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            var textColumn = e.Column as DataGridTextColumn;
+            if (textColumn == null)
+                return;
+
+            var style = new Style(typeof(DataGridCell));
+            var trigger = new DataTrigger() { Binding = textColumn.Binding };
+            trigger.Value = "";
+            trigger.Setters.Add(new Setter(BackgroundProperty, Brushes.Salmon));
+            style.Triggers.Add(trigger);
+            textColumn.CellStyle = style;
         }
     }
 }
