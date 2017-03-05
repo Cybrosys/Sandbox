1341d71b28cf0d0785f9d2fd7d077961ca56beae
Mikael Nensén <Mikael Nensn>
2017-03-05 01:24:00 +0100

    Renamed test file.

---
+++ Lottery/test/Lottery.Tests/RangeNumberGeneratorTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Lottery.Data;
+
+namespace Lottery.Tests
+{
+    public class RangeNumberGeneratorTests
+    {
+        [Fact]
+        public void Generate_only_middle_range_numbers() 
+        {
+            // Arrange
+            byte min = 1;
+            byte max = 1;
+            var generator = new RangeNumberGenerator(new Data.Random());
+
+            // Act
+            var number = generator.Generate(min, max);
+
+            // Assert
+            Assert.True(number.Value == 1);
+            Assert.True(number.IsOdd);
+            Assert.True(number.IsMiddle);
+        }
+    }
+}
