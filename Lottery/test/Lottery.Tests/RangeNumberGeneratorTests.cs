934b5a371d053b9a96fd4df93bdaf4d9b7a943ea
Mikael Nensén <Mikael Nensn>
2017-03-05 01:30:45 +0100

    Some basic tets.

--- Lottery/test/Lottery.Tests/RangeNumberGeneratorTests.cs
+++ Lottery/test/Lottery.Tests/RangeNumberGeneratorTests.cs
@@ -5,21 +5,68 @@ namespace Lottery.Tests
 {
     public class RangeNumberGeneratorTests
     {
+        private RangeNumberGenerator _generator;
+        private RangeNumberGenerator Generator => _generator ?? (_generator = new RangeNumberGenerator(new Data.Random()));
+
         [Fact]
-        public void Generate_only_middle_range_numbers() 
+        public void Generate_odd_middle_number() 
         {
             // Arrange
             byte min = 1;
             byte max = 1;
-            var generator = new RangeNumberGenerator(new Data.Random());
 
             // Act
-            var number = generator.Generate(min, max);
+            var number = Generator.Generate(min, max);
 
             // Assert
             Assert.True(number.Value == 1);
             Assert.True(number.IsOdd);
+            Assert.False(number.IsEven);
+            Assert.False(number.IsLower);
             Assert.True(number.IsMiddle);
+            Assert.False(number.IsUpper);
+        }
+
+        [Fact]
+        public void Generate_odd_lower_number()
+        {
+            // Arrange
+            byte min = 1;
+            byte max = 1;
+            byte minRange = 1;
+            byte maxRange = byte.MaxValue;
+
+            // Act
+            var number = Generator.Generate(min, max, minRange, maxRange);
+
+            // Assert
+            Assert.True(number.Value == 1);
+            Assert.True(number.IsOdd);
+            Assert.False(number.IsEven);
+            Assert.True(number.IsLower);
+            Assert.False(number.IsMiddle);
+            Assert.False(number.IsUpper);
+        }
+
+        [Fact]
+        public void Generate_odd_upper_number()
+        {
+            // Arrange
+            byte min = byte.MaxValue;
+            byte max = byte.MaxValue;
+            byte minRange = 1;
+            byte maxRange = byte.MaxValue;
+
+            // Act
+            var number = Generator.Generate(min, max, minRange, maxRange);
+
+            // Assert
+            Assert.True(number.Value == byte.MaxValue);
+            Assert.True(number.IsOdd);
+            Assert.False(number.IsEven);
+            Assert.False(number.IsLower);
+            Assert.False(number.IsMiddle);
+            Assert.True(number.IsUpper);
         }
     }
 }
