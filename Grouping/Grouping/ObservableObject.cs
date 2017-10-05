78ecb33fccdb3ea2a25f33cbe7453ba0a1758a81
Mikael Nensén <cybrosys@hotmail.com>
2017-10-05 18:02:42 +0200

    Initial commit

---
+++ Grouping/Grouping/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Grouping
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual bool SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (ReferenceEquals(property, value) || EqualityComparer<T>.Default.Equals(property, value))
+                return false;
+            property = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
---
+++ Grouping/Grouping/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grouping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+ObservableGroupingCollection
+	Normal collection changed events are because of the groups, their sort order is based
+	on their keys.
+	Additional events are to notify about when items are added/removed from/to groups,
+	and moved between groups.
+             */
+
+            Expression<Func<DateTime, int>> x = item => item.Minute;
+            var func = x.Compile();
+            Console.WriteLine(func(DateTime.Now));
+
+            var collection = new ObservableOrderedCollection<DateTime, int>(new DateTime[] { }, item => item.Minute);
+            
+        }
+    }
+}
