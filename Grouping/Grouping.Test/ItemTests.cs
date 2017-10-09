7ecd230aa3105dacd78412f5f84bc3da49747e48
Mikael Nensén <cybrosys@hotmail.com>
2017-10-09 10:22:17 +0200

    Some more property tests.

--- Grouping/Grouping.Test/ItemTests.cs
+++ Grouping/Grouping.Test/ItemTests.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace Grouping.Test
 {
     [TestClass]
-    public class ItemTests : DefaultImplTests<Item>
+    public class ItemIntPropertyTests : DefaultImplTests<Item>
     {
         public override void PrepareData()
         {
@@ -25,6 +25,46 @@ namespace Grouping.Test
                 });
             }
             _sortedData = _data.OrderBy(item => item.Index).ToList();
+        }
+    }
+
+    [TestClass]
+    public class ItemStringPropertyTests : DefaultImplTests<Item>
+    {
+        public override void PrepareData()
+        {
+            _data = new List<Item>(100);
+            _orderBy = nameof(Item.Name);
+            for (int i = 0; i < 100; ++i)
+            {
+                _data.Add(new Item
+                {
+                    Index = _random.Next() % 100,
+                    Name = _random.Next().ToString(),
+                    Timestamp = new DateTime(_random.Next(2000, 2018), _random.Next(1, 13), _random.Next(1, 28))
+                });
+            }
+            _sortedData = _data.OrderBy(item => item.Name).ToList();
+        }
+    }
+
+    [TestClass]
+    public class ItemDateTimePropertyTests : DefaultImplTests<Item>
+    {
+        public override void PrepareData()
+        {
+            _data = new List<Item>(100);
+            _orderBy = nameof(Item.Timestamp);
+            for (int i = 0; i < 100; ++i)
+            {
+                _data.Add(new Item
+                {
+                    Index = _random.Next() % 100,
+                    Name = _random.Next().ToString(),
+                    Timestamp = new DateTime(_random.Next(2000, 2018), _random.Next(1, 13), _random.Next(1, 28))
+                });
+            }
+            _sortedData = _data.OrderBy(item => item.Timestamp).ToList();
         }
     }
 }
