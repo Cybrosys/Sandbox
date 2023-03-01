9e26f760f58abc3c85da0004b14c2adba79602f8
Mikael Nensén <mikael.nensen@datema.se>
2023-03-02 00:03:24 +0100

    Initial commit of repro sample.

---
+++ MauiAppStatusBarColorDynamicResource/MauiAppStatusBarColorDynamicResource/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppStatusBarColorDynamicResource;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiAppStatusBarColorDynamicResource/MauiAppStatusBarColorDynamicResource/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppStatusBarColorDynamicResource;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
